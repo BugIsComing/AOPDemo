0ee0ff9ddf94a46b85871392bbcecd040aa08586
LC <LC@example.com>
2017-11-09 22:31:54 +0800

    1.03
    添加log4j的使用，接下来需要理解log4j.properties文件具体含义

--- src/main/java/com/lc/aop/MyAop.java
+++ src/main/java/com/lc/aop/MyAop.java
@@ -1,5 +1,8 @@
 package com.lc.aop;
+import org.apache.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
+
+
 
 /**
  * @Author:LC
@@ -8,6 +11,7 @@ import org.aspectj.lang.ProceedingJoinPoint;
  */
 
 public class MyAop {
+    private Logger logger = Logger.getLogger(MyAop.class);
     public MyAop(){
 
     }
@@ -20,23 +24,34 @@ public class MyAop {
 
     public Object process(ProceedingJoinPoint point){
         Object proc = null;
-        System.out.println("调用方法:" + point.getSignature().getName());
-        System.out.println("入参是:");
+        logger.info("调用方法:" + point.getSignature().getName());
+        //System.out.println("调用方法:" + point.getSignature().getName());
+        //System.out.print("入参是:");
+        logger.info("入参是:");
         Object []args = point.getArgs();
         String argName = null;
-        String argValue = null;
-        for (int i = 0; i<args.length; i++){
-            argName = args[i].toString();
-            System.out.println(argName);
+
+        if(args == null || args.length == 0){
+            //System.out.println("NULL");
+            logger.info("NULL");
+        } else {
+            for (int i = 0; i<args.length; i++){
+                argName = args[i].toString();
+                //System.out.println(argName);
+                logger.info(argName);
+            }
         }
-        System.out.println("出参是:" );
+        //System.out.print("出参是:" );
+        logger.info("出参是:");
         try {
             //目标方法有返回才不为null，否则为null
            proc = point.proceed();
            if(null != proc){
-               System.out.println(proc.toString());
+               //System.out.println(proc.toString());
+               logger.info(proc.toString());
            } else {
-               System.out.println("NULL");
+               //System.out.println("NULL");
+               logger.info("NULL");
            }
 
         } catch (Throwable throwable) {
