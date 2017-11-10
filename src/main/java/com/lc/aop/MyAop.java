d27362aa15e900cf424135d91f3d08a3c63bd85b
刘诚 <LC513901@163.com>
2017-11-10 13:37:29 +0800

    1.03
    了解log4j配置文件

--- src/main/java/com/lc/aop/MyAop.java
+++ src/main/java/com/lc/aop/MyAop.java
@@ -25,33 +25,41 @@ public class MyAop {
     public Object process(ProceedingJoinPoint point){
         Object proc = null;
         logger.info("调用方法:" + point.getSignature().getName());
+        //如果log4j.properties文件中的级别试着为debug，则输出，否则不输出
+        logger.debug("调用方法:" + point.getSignature().getName());
         //System.out.println("调用方法:" + point.getSignature().getName());
         //System.out.print("入参是:");
         logger.info("入参是:");
+        logger.debug("入参是:");
         Object []args = point.getArgs();
         String argName = null;
 
         if(args == null || args.length == 0){
             //System.out.println("NULL");
             logger.info("NULL");
+            logger.debug("NULL");
         } else {
             for (int i = 0; i<args.length; i++){
                 argName = args[i].toString();
                 //System.out.println(argName);
                 logger.info(argName);
+                logger.debug(argName);
             }
         }
         //System.out.print("出参是:" );
         logger.info("出参是:");
+        logger.debug("出参是:");
         try {
             //目标方法有返回才不为null，否则为null
            proc = point.proceed();
            if(null != proc){
                //System.out.println(proc.toString());
                logger.info(proc.toString());
+               logger.debug(proc.toString());
            } else {
                //System.out.println("NULL");
                logger.info("NULL");
+               logger.debug("NULL");
            }
 
         } catch (Throwable throwable) {
